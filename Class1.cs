fad47ec45da9645dca89306f1bf8cd395aa1adfe
Jose Claudio G. Vanni <45380252+Jcgvanni@users.noreply.github.com>
2020-06-24 09:44:20 -0700

    Add files via upload
    Program to give a partNumber, part Description, Quantity, and Price.
    Print a invoice information.

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01___300295374
+{
+    class Invoice
+    {
+        public string PartNumber { get; set; }
+        public string PartDesc { get; set; }
+        private int Quantity;
+        private decimal PricePerItem;
+
+        public Invoice(string partNumber, string partDesc, int quantity, decimal pricePerItem)
+        {
+            PartNumber = partNumber;
+            PartDesc = partDesc;
+            Quantity = quantity;
+            PricePerItem = pricePerItem;
+        }
+
+        public int getQuantity
+        {
+            get { return Quantity;  }
+            set
+            {
+                if (value > 0)
+                    Quantity = value;
+                else
+                    Quantity = 1;
+            }
+            
+        }
+        public decimal getUnitPrice
+        {
+            get { return PricePerItem;  }
+            set
+            {
+                if (value > 0)
+                    PricePerItem = value;
+                else
+                    PricePerItem = 1;
+            }
+
+        }
+        public decimal GetInvoiceAmount()
+        {
+            return getQuantity * getUnitPrice;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01___300295374
+{
+    /* Student: Jose Claudio G. Vanni
+     * ID: 300295374
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    string partNumber, partDesc;
+                    int quant;
+                    decimal price;
+
+
+                    Console.WriteLine("Please enter part Number: ");
+                    partNumber = Console.ReadLine();
+                    Console.WriteLine("Please enter part description: ");
+                    partDesc = Console.ReadLine();
+                    Console.WriteLine("Please enter quantity: ");
+                    quant = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter Price: ");
+                    price = decimal.Parse(Console.ReadLine());
+
+                    Invoice invoice = new Invoice(partNumber, partDesc, quant, price);
+
+                    Console.WriteLine("Original invoice information ");
+                    Console.WriteLine($"Part number: {invoice.PartNumber}");
+                    Console.WriteLine($"Description: {invoice.PartDesc}");
+                    Console.WriteLine($"Quantity: {invoice.getQuantity}");
+                    Console.WriteLine($"Price: {invoice.getUnitPrice:C}");
+                    Console.WriteLine($"Invoice amount: {invoice.GetInvoiceAmount():C}");
+                    Console.ReadLine();
+
+                    Console.WriteLine("Another invoice or type quit to exit");
+                    string input = Console.ReadLine();
+                    if (input == "quit")
+                    { 
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter part Number: ");
+                        partNumber = Console.ReadLine();
+                        Console.WriteLine("Please enter part description: ");
+                        partDesc = Console.ReadLine();
+                        Console.WriteLine("Please enter quantity: ");
+                        quant = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter Price: ");
+                        price = decimal.Parse(Console.ReadLine());
+                        Console.ReadLine();
+                    }
+                       
+                    
+
+                    Console.WriteLine("Updated invoice information ");
+                    Console.WriteLine($"Part number: {invoice.PartNumber}");
+                    Console.WriteLine($"Description: {invoice.PartDesc}");
+                    Console.WriteLine($"Quantity: {invoice.getQuantity}");
+                    Console.WriteLine($"Price: {invoice.getUnitPrice:C}");
+                    Console.WriteLine($"Invoice amount: {invoice.GetInvoiceAmount():C}");                    
+                }
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }    
+        }
+    }
+}
