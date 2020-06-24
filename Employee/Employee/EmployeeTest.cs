6f6864cac5c27de2e6f0c8bd83f5260678a23c1b
Jose Claudio G. Vanni <45380252+Jcgvanni@users.noreply.github.com>
2020-06-24 09:52:48 -0700

    Add files via upload
    Program to run employees salary and raises

---
+++ Employee/Employee/EmployeeTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Employee
+{
+    class EmployeeTest    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private decimal salary;
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    salary = value;
+                }
+            }
+        }
+        public EmployeeTest(string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }       
+    }
+}
+
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeTest employee1 = new EmployeeTest("Bob", "Jones", 34500);
+            EmployeeTest employee2 = new EmployeeTest("Susan", "Baker", 37809);
+
+            Console.WriteLine("Employee 1: {0} {1}; Yearly Salary: {2:C2} ", employee1.FirstName, employee1.LastName, employee1.Salary);
+            Console.WriteLine("Employee 2: {0} {1}; Yearly Salary: {2:C2} ", employee2.FirstName, employee2.LastName, employee2.Salary);
+            Console.WriteLine();
+
+            Console.WriteLine("Increasing employee salaries by 10%");
+            employee1.Salary *= (decimal) 1.1;
+            employee2.Salary *= (decimal) 1.1;
+            Console.WriteLine("Employee 1: {0} {1}; Yearly Salary: {2:C2} ", employee1.FirstName, employee1.LastName, employee1.Salary);
+            Console.WriteLine("Employee 2: {0} {1}; Yearly Salary: {2:C2} ", employee2.FirstName, employee2.LastName, employee2.Salary);
+
+            Console.ReadLine();
+        }
+    }
+}
