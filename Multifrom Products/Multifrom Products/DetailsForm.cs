949cacf4b33ca6e5af61539625f2bc6054724f15
Jose Claudio G. Vanni <45380252+Jcgvanni@users.noreply.github.com>
2020-06-24 10:30:50 -0700

    Add files via upload
    Add, delete, update items from the table.
    GUI form

---
+++ Multifrom Products/Multifrom Products/DetailsForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multifrom_Products
+{
+    public partial class DetailsForm : Form
+    {
+        public DetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Details_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Multifrom Products/Multifrom Products/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multifrom_Products
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+
+        }
+
+        private void showDetailsButton_Click(object sender, EventArgs e)
+        {
+            //create an instance of the detailsform
+            DetailsForm details = new DetailsForm();
+
+            //display the form
+            details.ShowDialog();
+
+            //update database
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close form
+            this.Close();
+        }
+    }
+}
